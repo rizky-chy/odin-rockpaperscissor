8687646d83847db05add64d1988aa91ca2c80782
rizky <rizkycp11@gmail.com>
2025-12-16 14:12:28 +0700

    Continue the project, continue working the logic logic finish, completing to the task assignment project finish console.log working as wanted

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
+let gameRounds = 1;
 
 function alertConnect(){
     console.log("Connected!!!");
@@ -20,40 +21,57 @@ function getComputerChoice(max) {
 }
 
 function getHumanchoice(){
-    choice = window.prompt("what is your choice?");
+    choice = window.prompt("Paper, Rock, Scissor Game!!!\nWhat Is Your Choice?");
     return choice;
 }
    
+
+function buttonAction(){
+    for (let i = 0; i < 5; i++) {
+    console.log(playGame());
+    console.log("Your Score: "+humanScore);
+    console.log("Computer Score: "+computerScore);
+}
+}
+
+function playGame(){
     const compResult = getComputerChoice(3);
-    const humanResult = getHumanchoice();
+    const humanInput = getHumanchoice();
+    const humanResult = humanInput.charAt(0).toUpperCase() + humanInput.slice(1).toLowerCase();
+    console.log("Round: "+gameRounds);
     console.log("Computer Choice: "+compResult);
     console.log("Your Choice: "+humanResult);
 
-function playRound(humanChoice, computerChoice){
+
     let result = "";
 
-    if(compResult==="Rock" && humanResult==="Paper" || 
+    if(compResult==="Rock" && humanResult=="Paper" || 
         compResult==="Paper" && humanResult==="Scissor" || 
         compResult==="Scissor" && humanResult==="Rock"
-    ){result = "You Win! " + humanResult + " Beats " + compResult, humanScore++;
-
+    ){result = "You Win! " + humanResult + " Beats " + compResult; 
+        humanScore++;
+        
     }else if(compResult==="Rock" && humanResult==="Scissor" || 
         compResult==="Paper" && humanResult==="Rock" || 
         compResult==="Scissor" && humanResult==="Paper" 
-    ){result = "You Lose! " + compResult + " Beats " + humanResult, computerScore;
+    ){result = "You Lose! " + compResult + " Beats " + humanResult; 
+        computerScore++;
+    
+    }else if(compResult === humanResult 
+    ){result = "Draw! Same Choice!"; 
+         
         
-    }else{result = "Draw! Same Choice!";}
+    }else{result = "Invalid Choice!!";
+        alert("Invalid Choice!!")
+    }
     
+    gameRounds++;
     return result;
-    
 }
 
-// playRound(compResult,humanResult);
-
-console.log(playRound());
-console.log("Your Score: "+humanScore);
-console.log("Computer Score: "+computerScore);
-
-function playGame(){
-    
+function reset(){
+    console.clear();
+    gameRounds = 1;
+    computerScore = 0;
+    humanScore = 0;
 }
